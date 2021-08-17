6fefeee786f2f19c32fc909511eca58c9a59545a
mohamed.jellali <mohamed.jellali@oxilog.net>
2021-08-17 14:38:42 +0100

    QUEUES api telco services

--- api/app/telco_api_services/api.js
+++ api/app/telco_api_services/api.js
@@ -3,14 +3,19 @@ var axios = require("axios").default
 const router = express.Router();
 const agent_token = require(__dirname + '/../config/config.json')["agent_token"];
 const trunck_token = require(__dirname + '/../config/config.json')["trunck_token"];
+const queue_token = require(__dirname + '/../config/config.json')["queue_token"];
 const base_url_agents = require(__dirname + '/../config/config.json')["base_url_agents"];
 const base_url_truncks = require(__dirname + '/../config/config.json')["base_url_truncks"];
+const base_url_queues = require(__dirname + '/../config/config.json')["base_url_queues"];
 
 const agents_authorization = {
   headers: { Authorization: agent_token }
 };
 const truncks_authorization = {
   headers: { Authorization: trunck_token }
+};
+const queues_authorization = {
+  headers: { Authorization: queue_token }
 };
 
 // to fix SSL certifcate Problem
@@ -99,6 +104,49 @@ let apiServices = function () {
      let id = req.params.id;
         axios
          .delete(`${base_url_truncks}api/v1/dialer/gateways/${id}`, truncks_authorization)
+         .then(resp => res.json(resp.data))
+         .catch(err => res.json(err))
+        })
+
+// **** QUEUES ****
+
+      //find all
+      router.get("/apicallcenter/queues", (req, res, next) => {
+        axios
+         .get(`${base_url_queues}api/v1/queues`, queues_authorization)
+         .then(resp => res.json(resp.data.result))
+         .catch(err => res.json(err))
+ })
+       //find by UUID
+ router.get("/apicallcenter/queues/:id", (req, res, next) => {
+     let id = req.params.id;
+        axios
+         .get(`${base_url_queues}api/v1/queues/${id}`, queues_authorization)
+         .then(resp => res.json(resp.data.result))
+         .catch(err => res.json(err))
+ })
+       //create new item
+ router.post("/apicallcenter/queues", (req, res, next) => {
+        axios
+         .post(`${base_url_queues}api/v1/queues`, req.body, queues_authorization)
+         .then(resp => {
+             res.json(resp.data);
+         })
+         .catch(err => res.json(err))
+ })
+       //modify item
+ router.put("/apicallcenter/queues/:id", (req, res, next) => {
+     let id = req.params.id;
+        axios
+         .put(`${base_url_queues}api/v1/queues/${id}`, req.body, queues_authorization)
+         .then(resp => res.json(resp.data))
+         .catch(err => res.json(err))
+ })
+       //delete item
+ router.delete("/apicallcenter/queues/:id", (req, res, next) => {
+     let id = req.params.id;
+        axios
+         .delete(`${base_url_queues}api/v1/queues/${id}`, queues_authorization)
          .then(resp => res.json(resp.data))
          .catch(err => res.json(err))
  })
