c9b7b5966b80ea507e9dbec6cbfa68251c44aa12
mohamed.jellali <mohamed.jellali@oxilog.net>
2021-08-20 17:41:35 +0100

    api telco : add acls, dispatcher, domains,rtpengine and servers routes

--- api/app/telco_api_services/api.js
+++ api/app/telco_api_services/api.js
@@ -3,19 +3,17 @@ var axios = require("axios").default
 const router = express.Router();
 const agent_token = require(__dirname + '/../config/config.json')["agent_token"];
 const trunck_token = require(__dirname + '/../config/config.json')["trunck_token"];
-const queue_token = require(__dirname + '/../config/config.json')["queue_token"];
 const base_url_agents = require(__dirname + '/../config/config.json')["base_url_agents"];
 const base_url_truncks = require(__dirname + '/../config/config.json')["base_url_truncks"];
 const base_url_queues = require(__dirname + '/../config/config.json')["base_url_queues"];
+const base_url_rest_api = require(__dirname + '/../config/config.json')["base_url_rest_api"];
+const base_url_server = require(__dirname + '/../config/config.json')["base_url_server"];
 
 const agents_authorization = {
   headers: { Authorization: agent_token }
 };
 const truncks_authorization = {
   headers: { Authorization: trunck_token }
-};
-const queues_authorization = {
-  headers: { Authorization: queue_token }
 };
 
 // to fix SSL certifcate Problem
@@ -113,7 +111,7 @@ let apiServices = function () {
       //find all
       router.get("/apicallcenter/queues", (req, res, next) => {
         axios
-         .get(`${base_url_queues}api/v1/queues`, queues_authorization)
+         .get(`${base_url_queues}api/v1/queues`, agents_authorization)
          .then(resp => res.json(resp.data.result))
          .catch(err => res.json(err))
  })
@@ -121,14 +119,14 @@ let apiServices = function () {
  router.get("/apicallcenter/queues/:id", (req, res, next) => {
      let id = req.params.id;
         axios
-         .get(`${base_url_queues}api/v1/queues/${id}`, queues_authorization)
+         .get(`${base_url_queues}api/v1/queues/${id}`, agents_authorization)
          .then(resp => res.json(resp.data.result))
          .catch(err => res.json(err))
  })
        //create new item
  router.post("/apicallcenter/queues", (req, res, next) => {
         axios
-         .post(`${base_url_queues}api/v1/queues`, req.body, queues_authorization)
+         .post(`${base_url_queues}api/v1/queues`, req.body, agents_authorization)
          .then(resp => {
              res.json(resp.data);
          })
@@ -138,7 +136,7 @@ let apiServices = function () {
  router.put("/apicallcenter/queues/:id", (req, res, next) => {
      let id = req.params.id;
         axios
-         .put(`${base_url_queues}api/v1/queues/${id}`, req.body, queues_authorization)
+         .put(`${base_url_queues}api/v1/queues/${id}`, req.body, agents_authorization)
          .then(resp => res.json(resp.data))
          .catch(err => res.json(err))
  })
@@ -146,10 +144,230 @@ let apiServices = function () {
  router.delete("/apicallcenter/queues/:id", (req, res, next) => {
      let id = req.params.id;
         axios
-         .delete(`${base_url_queues}api/v1/queues/${id}`, queues_authorization)
+         .delete(`${base_url_queues}api/v1/queues/${id}`, agents_authorization)
          .then(resp => res.json(resp.data))
          .catch(err => res.json(err))
  })
+
+ // **** acl_groups ****
+
+      //find all
+      router.get("/apicallcenter/acl_groups", (req, res, next) => {
+            axios
+             .get(`${base_url_rest_api}api/v1/acl_groups`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //find by UUID
+     router.get("/apicallcenter/acl_groups/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .get(`${base_url_rest_api}api/v1/acl_groups/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //create new item
+     router.post("/apicallcenter/acl_groups", (req, res, next) => {
+            axios
+             .post(`${base_url_rest_api}api/v1/acl_groups`, req.body, agents_authorization)
+             .then(resp => {
+                 res.json(resp.data.result);
+             })
+             .catch(err => res.json(err))
+     })
+           //modify item
+     router.put("/apicallcenter/acl_groups/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .put(`${base_url_rest_api}api/v1/acl_groups/${id}`, req.body, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+           //delete item
+     router.delete("/apicallcenter/acl_groups/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .delete(`${base_url_rest_api}api/v1/acl_groups/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+
+ // **** dispatcher ****
+
+      //find all
+      router.get("/apicallcenter/dispatcher", (req, res, next) => {
+            axios
+             .get(`${base_url_rest_api}api/v1/dispatcher`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //find by UUID
+     router.get("/apicallcenter/dispatcher/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .get(`${base_url_rest_api}api/v1/dispatcher/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //create new item
+     router.post("/apicallcenter/dispatcher", (req, res, next) => {
+            axios
+             .post(`${base_url_rest_api}api/v1/dispatcher`, req.body, agents_authorization)
+             .then(resp => {
+                 res.json(resp.data);
+             })
+             .catch(err => res.json(err))
+     })
+           //modify item
+     router.put("/apicallcenter/dispatcher/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .put(`${base_url_rest_api}api/v1/dispatcher/${id}`, req.body, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+           //delete item
+     router.delete("/apicallcenter/dispatcher/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .delete(`${base_url_rest_api}api/v1/dispatcher/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+
+
+
+ // **** domains ****
+
+      //find all
+      router.get("/apicallcenter/domains", (req, res, next) => {
+            axios
+             .get(`${base_url_rest_api}api/v1/domains`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //find by UUID
+     router.get("/apicallcenter/domains/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .get(`${base_url_rest_api}api/v1/domains/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //create new item
+     router.post("/apicallcenter/domains", (req, res, next) => {
+            axios
+             .post(`${base_url_rest_api}api/v1/domains`, req.body, agents_authorization)
+             .then(resp => {
+                 res.json(resp.data);
+             })
+             .catch(err => res.json(err))
+     })
+           //modify item
+     router.put("/apicallcenter/domains/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .put(`${base_url_rest_api}api/v1/domains/${id}`, req.body, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+           //delete item
+     router.delete("/apicallcenter/domains/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .delete(`${base_url_rest_api}api/v1/domains/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+
+
+ // **** rtpengine ****
+
+      //find all
+      router.get("/apicallcenter/rtpengine", (req, res, next) => {
+            axios
+             .get(`${base_url_rest_api}api/v1/rtpengine`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //find by UUID
+     router.get("/apicallcenter/rtpengine/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .get(`${base_url_rest_api}api/v1/rtpengine/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //create new item
+     router.post("/apicallcenter/rtpengine", (req, res, next) => {
+            axios
+             .post(`${base_url_rest_api}api/v1/rtpengine`, req.body, agents_authorization)
+             .then(resp => {
+                 res.json(resp.data);
+             })
+             .catch(err => res.json(err))
+     })
+           //modify item
+     router.put("/apicallcenter/rtpengine/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .put(`${base_url_rest_api}api/v1/rtpengine/${id}`, req.body, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+           //delete item
+     router.delete("/apicallcenter/rtpengine/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .delete(`${base_url_rest_api}api/v1/rtpengine/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+
+
+
+ // **** servers ****
+
+      //find all
+      router.get("/apicallcenter/servers", (req, res, next) => {
+            axios
+             .get(`${base_url_server}api/v1/servers`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //find by UUID
+     router.get("/apicallcenter/servers/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .get(`${base_url_server}api/v1/servers/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data.result))
+             .catch(err => res.json(err))
+     })
+           //create new item
+     router.post("/apicallcenter/servers", (req, res, next) => {
+            axios
+             .post(`${base_url_server}api/v1/servers`, req.body, agents_authorization)
+             .then(resp => {
+                 res.json(resp.data);
+             })
+             .catch(err => res.json(err))
+     })
+           //modify item
+     router.put("/apicallcenter/servers/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .put(`${base_url_server}api/v1/servers/${id}`, req.body, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
+           //delete item
+     router.delete("/apicallcenter/servers/:id", (req, res, next) => {
+         let id = req.params.id;
+            axios
+             .delete(`${base_url_server}api/v1/servers/${id}`, agents_authorization)
+             .then(resp => res.json(resp.data))
+             .catch(err => res.json(err))
+     })
 
 
     return router
