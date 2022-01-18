3bd21b9b6fc0cea2c6e11f5eaa01c0232f8dc44b
Oussama SAAD <oussama.saad@oxilog.net>
2022-01-18 03:57:36 -0500

    merge code

--- api/app/bo/usersbo.js
+++ api/app/bo/usersbo.js
@@ -23,6 +23,9 @@ class users extends baseModelbo {
             const {username} = req.body;
             if (username) {
                 this.db['users'].findOne({
+                    include: [{
+                        model: db.roles_crms,
+                    }],
                     where: {
                         username: username,
                         active: 'Y'
@@ -58,89 +61,6 @@ class users extends baseModelbo {
         }
     }
 
-    signIn(req, res, next) {
-        let _this = this;
-        if ((!req.body.username || !req.body.password)) {
-            return _this.sendResponseError(res, ['Error.RequestDataInvalid'], 0, 403);
-        } else {
-            const {username, password} = req.body;
-            if (username && password) {
-                this.db['users'].findOne({
-                    where: {
-                        username: username,
-                        active: 'Y'
-                    }
-                }).then((user) => {
-                    if (!user) {
-                        _this.sendResponseError(res, ['Error.UserNotFound'], 0, 403);
-                    } else {
-                        let password_hashed = user.password_hash;
-                        bcrypt.compare(password, password_hashed)
-                            .then(validPassword => {
-                                if (validPassword) {
-                                    // this.db['has_permissions'].findAll({
-                                    //     include: [{
-                                    //         model: db.permissions,
-                                    //         as: 'permission',
-                                    //     }],
-                                    //     where: {
-                                    //         role_id: user.role_id,
-                                    //     }
-                                    // }).then(function (permissions) {
-                                    //     _this.getPermissionsValues(permissions).then(data_perm => {
-                                    const token = jwt.sign({
-                                            user_id: user.user_id,
-                                            username: user.username
-                                        },
-                                        appSecret, {
-                                            expiresIn: '8600m'
-                                        });
-
-                                    res.send({
-                                        message: 'Success',
-                                        user: user.toJSON(),
-                                        // permissions: data_perm || [],
-                                        token: token,
-                                        result: 1,
-                                        success: true,
-                                    });
-                                    //     })
-                                    // })
-
-                                } else {
-                                    _this.sendResponseError(res, ['Error.InvalidPassword'], 2, 403);
-                                }
-                            })
-                            .catch((error) => {
-                                return _this.sendResponseError(res, ['Error.AnErrorHasOccuredUser', error], 1, 403);
-                            });
-                    }
-                }).catch((error) => {
-                    return _this.sendResponseError(res, ['Error.AnErrorHasOccuredUser', error], 1, 403);
-                });
-            }
-        }
-    }
-
-    getPermissionsValues = (permissions) => {
-        return new Promise((resolve, reject) => {
-            if (permissions && permissions.length !== 0) {
-                let permissions_values = [];
-                let index = 0;
-                permissions.forEach(item_perm => {
-                    permissions_values.push(item_perm.permission.value);
-                    if (index < permissions.length - 1) {
-                        index++
-                    } else {
-                        resolve(permissions_values);
-                    }
-                })
-            } else {
-                resolve([]);
-            }
-        })
-    }
-
     saveUser(req, res, next) {
         let _this = this;
         let newAccount = req.body;
@@ -159,8 +79,7 @@ class users extends baseModelbo {
     async saveUserFunction(newAccount) {
         let _this = this;
         if (newAccount.password_hash) {
-            const hashed_password = await bcrypt.hash(newAccount.password_hash, salt)
-            newAccount.password_hash = hashed_password;
+            newAccount.password_hash = await bcrypt.hash(newAccount.password_hash, salt);
         }
         return new Promise((resolve, reject) => {
             if (newAccount.user_id) {
@@ -263,7 +182,6 @@ class users extends baseModelbo {
     }
 
     signIn(req, res, next) {
-
         if ((!req.body.username || !req.body.password)) {
             return this.sendResponseError(res, ['Error.RequestDataInvalid'], 0, 403);
         } else {
@@ -278,6 +196,7 @@ class users extends baseModelbo {
                         active: 'Y'
                     }
                 }).then((user) => {
+                    console.log(user)
                     if (!user) {
 
                         //this.sendResponseError(res, ['Error.UserNotFound'], 0, 403);
@@ -295,8 +214,8 @@ class users extends baseModelbo {
                                 where: {
                                     roles_crm_id: user.role_crm_id,
                                 }
-                            }).then(permissions => {
-                                this.getPermissionsValues(permissions).then(data_perm => {
+                            }).then(permissions =>{
+                                this.getPermissionsValues(permissions).then(data_perm =>{
                                     const token = jwt.sign({
                                         user_id: user.user_id,
                                         username: user.username,
@@ -320,6 +239,7 @@ class users extends baseModelbo {
                         }
                     }
                 }).catch((error) => {
+                    console.log(error)
                     return this.sendResponseError(res, ['Error.AnErrorHasOccuredUser'], 1, 403);
                 });
             }
