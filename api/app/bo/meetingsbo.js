ed9b28d920999959aecbe9bdfd73001af81e1fce
mohamed <mouhamed.jellali@oxilog.net>
2022-02-08 14:58:11 +0100

    fix add meetings

--- api/app/bo/meetingsbo.js
+++ api/app/bo/meetingsbo.js
@@ -20,15 +20,16 @@ class meetings extends baseModelbo {
         } else return false;
     }
 
-    isAvailableTime(day, first_day, last_day, availableDays) {
-        let meeting_day = day.day ? day.day : day;
-        let started_meeting_time = moment(meeting_day).format("HH:mm");
-        if (moment(meeting_day).isBetween(first_day, last_day)) {
-            return availableDays.includes(dayName);
-        } else return false;
+    isAvailableTime(meeting_start, meeting_end, first_day, last_day,) {
+        let format = 'HH:mm:ss'
+        let start_work_hour = moment(first_day).format("HH:mm:ss");
+        let end_work_hour = moment(last_day).format("HH:mm:ss");
+        let first_condition = moment(meeting_start, format).isBetween(moment(start_work_hour, format), moment(end_work_hour, format));
+        let second_condition = moment(meeting_end, format).isBetween(moment(start_work_hour, format), moment(end_work_hour, format));
+        return first_condition && second_condition
     }
 
-    getAvailability(sales, day) {
+    getAvailability(sales, day, meeting_start, meeting_end) {
         return new Promise((resolve, reject) => {
             let sales_man = sales.toJSON();
             let first_day;
@@ -39,7 +40,11 @@ class meetings extends baseModelbo {
                 last_day = sales_man.params.availability.last_day;
                 days = sales_man.params.availability.days;
                 if (this.isAvailableDay(day, first_day[0], last_day[0], days)) {
-                    resolve(sales_man);
+                    if (this.isAvailableTime(meeting_start, meeting_end, first_day[0], last_day[0])) {
+                        resolve(sales_man);
+                    } else {
+                        resolve(null);
+                    }
                 } else {
                     resolve(null);
                 }
@@ -67,7 +72,10 @@ class meetings extends baseModelbo {
 
     getAvailableSales(req, res, next) {
         let _this = this;
-        let {day, agent_id} = req.body.date;
+        let agent_id = req.body.agent_id;
+        let {day, started_at, finished_at} = req.body.date;
+        let meeting_start = moment(started_at).format("HH:mm:ss");
+        let meeting_end = moment(finished_at).format("HH:mm:ss");
         this.db["users"]
             .findAll({
                 where: {
@@ -76,14 +84,14 @@ class meetings extends baseModelbo {
                 },
             })
             .then((list) => {
-                let list_of_sales_men = list.filter(sales => sales.params.agents.includes(agent_id))
+                let list_of_sales_men = list.filter(sales => sales.params.agents.includes(agent_id));
                 let availableSales = [];
                 let promise = [];
                 if (list_of_sales_men && list_of_sales_men.length !== 0) {
                     promise.push(new Promise(function (resolve, reject) {
                         let index = 0;
                         list_of_sales_men.forEach(sales => {
-                            _this.getAvailability(sales, day).then(availableSale => {
+                            _this.getAvailability(sales, day, meeting_start, meeting_end).then(availableSale => {
                                 if (availableSale) {
                                     availableSale.meetings = _this.getMeetings(availableSale.user_id) || [];
                                     availableSales.push(availableSale);
@@ -113,13 +121,13 @@ class meetings extends baseModelbo {
                 }
             })
             .catch((err) => {
-                let res = [];
-                let messages = "Cannot fetch data from database";
-                this.sendResponseError(res, messages, err, (status = 500));
+                return _this.sendResponseError(res, ['Error.AnErrorHasOccuredUser', err], 1, 403);
             });
     }
 
+    // to be cleaned
     saveMeetings(req, res, next) {
+        let _this = this;
         let sales_id = req.body.sales_id
         let started_at = req.body.started_at
         this.db["users"]
@@ -130,25 +138,11 @@ class meetings extends baseModelbo {
                 },
             })
             .then(result => {
-                let updated_event = result;
-                let duration = result.params.availability.duration;
-                let interval = result.params.availability.interval;
-                let totalTime = +duration + +interval;
-                let finished_at = moment.tz(started_at, tz).add(totalTime, 'minutes')
-
-                updated_event.finished_at = finished_at
-                this.save(updated_event)
-
-                return (res.send({
-                    message: "Success",
-                    success: true,
-                    result: {
-                        started_at: started_at,
-                        finished_at: finished_at,
-                        totalTime: totalTime
-                    },
-                }))
+                res.send(result)
             })
+            .catch((err) => {
+                return _this.sendResponseError(res, ['Error.AnErrorHasOccuredUser', err], 1, 403);
+            });
     }
 
 }
