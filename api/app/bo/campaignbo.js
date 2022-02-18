edcb57749e7293f61b406cc680875ea4ab6d2d1f
mohamed <mouhamed.jellali@oxilog.net>
2022-02-18 02:42:53 -0500

    campaign : delete meetings of unassigned agents

--- api/app/bo/campaignbo.js
+++ api/app/bo/campaignbo.js
@@ -107,14 +107,14 @@ class campaigns extends baseModelbo {
             });
     }
 
-    dissociateAgent(agents){
+    dissociateAgent(agents) {
         let index = 0;
         return new Promise((resolve, reject) => {
-            if(agents && agents.length !== 0) {
+            if (agents && agents.length !== 0) {
                 agents.forEach(agent_id => {
-                    this.db['users'].update({campaign_id : 0}, {where : {user_id : agent_id}})
-                        .then(resp =>  {
-                            if(index < agents.length - 1) {
+                    this.db['users'].update({campaign_id: 0}, {where: {user_id: agent_id}})
+                        .then(resp => {
+                            if (index < agents.length - 1) {
                                 index++;
                             } else {
                                 resolve(true);
@@ -141,7 +141,7 @@ class campaigns extends baseModelbo {
                     axios
                         .delete(`${base_url_cc_kam}api/v1/queues/${uuid}`, call_center_authorization)
                         .then(resp => {
-                            this.db['campaigns'].findOne({where : {campaign_id : campaign_id}})
+                            this.db['campaigns'].findOne({where: {campaign_id: campaign_id}})
                                 .then(campaign => {
                                     let agents = campaign.agents;
                                     this.dissociateAgent(agents)
@@ -194,8 +194,8 @@ class campaigns extends baseModelbo {
         this.addDefaultStatus(campaign_id)
             .then(resp => {
                 res.send({
-                    status : 200,
-                    message : "success"
+                    status: 200,
+                    message: "success"
                 })
             })
             .catch((err) => {
@@ -226,7 +226,7 @@ class campaigns extends baseModelbo {
             let index_callstatus = 0;
             this.getLookupsByType("DEFAULTCALLSTATUS")
                 .then((data) => {
-                    if(data && data.length !== 0) {
+                    if (data && data.length !== 0) {
                         data.forEach((el) => {
                             let obj = {};
                             obj.code = el.key;
@@ -262,7 +262,7 @@ class campaigns extends baseModelbo {
             let index_pausestatus = 0;
             this.getLookupsByType("DEFAULTPAUSESTATUS")
                 .then((data) => {
-                    if(data && data.length !== 0) {
+                    if (data && data.length !== 0) {
                         data.forEach((el) => {
                             let obj = {};
                             obj.code = el.key;
@@ -295,7 +295,7 @@ class campaigns extends baseModelbo {
 
     getLookupsByType(type) {
         return new Promise((resolve, reject) => {
-            this.db['lookups'].findAll({where : {type : type}})
+            this.db['lookups'].findAll({where: {type: type}})
                 .then(response => {
                     resolve(response)
                 })
@@ -457,19 +457,21 @@ class campaigns extends baseModelbo {
     getAssignedAgents(req, res, next) {
         let _this = this;
         let {campaign_id, account_id} = req.body;
-        this.db['campaigns'].findOne({where : {campaign_id : campaign_id}})
+        this.db['campaigns'].findOne({where: {campaign_id: campaign_id}})
             .then(campaign => {
-                this.db['users'].findAll({where : {
+                this.db['users'].findAll({
+                    where: {
                         role_crm_id: 3,
                         active: 'Y',
-                        account_id : account_id,
-                        $or: [{campaign_id: {$eq : campaign_id}}, {campaign_id: {$eq : 0}}],
-                    }})
+                        account_id: account_id,
+                        $or: [{campaign_id: {$eq: campaign_id}}, {campaign_id: {$eq: 0}}],
+                    }
+                })
                     .then(agents => {
                         let campAgents = campaign.agents ? campaign.agents : [];
                         let assignedAgents = [];
                         let notAssignedAgents = [];
-                        if(campAgents && campAgents.length !== 0) {
+                        if (campAgents && campAgents.length !== 0) {
                             assignedAgents = agents.filter((agent) => campAgents.includes(agent.user_id));
                             notAssignedAgents = agents.filter((agent) => !campAgents.includes(agent.user_id));
                         } else {
@@ -482,9 +484,9 @@ class campaigns extends baseModelbo {
                             campaign
                         }
                         res.send({
-                            status : 200,
-                            message : 'success',
-                            data : data
+                            status: 200,
+                            message: 'success',
+                            data: data
                         })
                     })
                     .catch((err) => {
@@ -499,11 +501,14 @@ class campaigns extends baseModelbo {
     updateIsAssignedStatus(agents, campaign_id, isAssigned) {
         let index = 0;
         return new Promise((resolve, reject) => {
-            if(agents && agents.length !== 0) {
+            if (agents && agents.length !== 0) {
                 agents.forEach(agent => {
-                    this.db['users'].update({isAssigned: isAssigned, campaign_id: campaign_id,},{where : {user_id : agent.user_id, active : 'Y'}})
+                    this.db['users'].update({
+                        isAssigned: isAssigned,
+                        campaign_id: campaign_id,
+                    }, {where: {user_id: agent.user_id, active: 'Y'}})
                         .then(resp => {
-                            if(index < agents.length - 1) {
+                            if (index < agents.length - 1) {
                                 index++;
                             } else {
                                 resolve(true);
@@ -521,7 +526,7 @@ class campaigns extends baseModelbo {
 
     addToQueue(tiers, queue_uuid) {
         return new Promise((resolve, reject) => {
-            if(tiers.tiers && tiers.tiers.length !== 0) {
+            if (tiers.tiers && tiers.tiers.length !== 0) {
                 axios
                     .post(`${base_url_cc_kam}api/v1/queues/${queue_uuid}/tiers`, tiers, call_center_authorization)
                     .then(resp => {
@@ -536,6 +541,29 @@ class campaigns extends baseModelbo {
         })
     }
 
+    deleteAgentsMeetings(agents) {
+        let index = 0;
+        return new Promise((resolve, reject) => {
+            if (agents && agents.length !== 0) {
+                agents.forEach(agent => {
+                    this.db['meetings'].update({active: 'N'}, {where: {agent_id: agent.user_id}})
+                        .then(result => {
+                            if (index < agents.length - 1) {
+                                index++;
+                            } else {
+                                resolve(true);
+                            }
+                        })
+                        .catch(err => {
+                            resolve(err);
+                        })
+                })
+            } else {
+                resolve(true)
+            }
+        })
+    }
+
     assignAgents(req, res, next) {
         let _this = this;
         let {campaign_id, agents, queue_uuid, assignedAgents, notAssignedAgents} = req.body;
@@ -544,26 +572,37 @@ class campaigns extends baseModelbo {
             .then(resp => {
                 this.updateIsAssignedStatus(notAssignedAgents, 0, false)
                     .then(resp => {
-                        let agents_arr = ['*'];
-                        let agents = {agents: agents_arr}
-                        axios
-                            .post(`${base_url_cc_kam}api/v1/queues/${queue_uuid}/tiers/delete`, agents, call_center_authorization)
-                            .then(resp => {
-                                let tiers_array = (assignedAgents && assignedAgents.length !== 0 ) ?
-                                    assignedAgents.map(el => ({
-                                    agent_uuid: el.sip_device.uuid,
-                                    tier_level: 1,
-                                    tier_position: 1
-                                })) : [];
-                                let tiers = {tiers: tiers_array};
-                                this.addToQueue(tiers, queue_uuid)
+                        this.deleteAgentsMeetings(notAssignedAgents)
+                            .then(result => {
+                                let agents_arr = ['*'];
+                                let agents = {agents: agents_arr}
+                                axios
+                                    .post(`${base_url_cc_kam}api/v1/queues/${queue_uuid}/tiers/delete`, agents, call_center_authorization)
                                     .then(resp => {
-                                        this.db['campaigns'].update(updates, {where : {active: 'Y', campaign_id: campaign_id}})
+                                        let tiers_array = (assignedAgents && assignedAgents.length !== 0) ?
+                                            assignedAgents.map(el => ({
+                                                agent_uuid: el.sip_device.uuid,
+                                                tier_level: 1,
+                                                tier_position: 1
+                                            })) : [];
+                                        let tiers = {tiers: tiers_array};
+                                        this.addToQueue(tiers, queue_uuid)
                                             .then(resp => {
-                                                res.send({
-                                                    status : 200,
-                                                    message : 'success'
+                                                this.db['campaigns'].update(updates, {
+                                                    where: {
+                                                        active: 'Y',
+                                                        campaign_id: campaign_id
+                                                    }
                                                 })
+                                                    .then(resp => {
+                                                        res.send({
+                                                            status: 200,
+                                                            message: 'success'
+                                                        })
+                                                    })
+                                                    .catch((err) => {
+                                                        return _this.sendResponseError(res, ['Error.AnErrorHasOccuredUser', err], 1, 403);
+                                                    });
                                             })
                                             .catch((err) => {
                                                 return _this.sendResponseError(res, ['Error.AnErrorHasOccuredUser', err], 1, 403);
