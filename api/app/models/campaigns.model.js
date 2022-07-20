eea12540235548601a49f6d8762e5adabab872bf
oussama <oussama.saad@oxilog.net>
2022-07-20 14:29:58 +0100

    add params camp

--- api/app/models/campaigns.model.js
+++ api/app/models/campaigns.model.js
@@ -1,85 +1,95 @@
 module.exports = (sequelize, Sequelize) => {
     const campaign = sequelize.define("campaigns", {
-        campaign_id : {
-            primaryKey : true,
-            autoIncrement: true,
-            type: Sequelize.INTEGER
+            campaign_id: {
+                primaryKey: true,
+                autoIncrement: true,
+                type: Sequelize.INTEGER
+            },
+            campaign_name: {
+                type: Sequelize.STRING
+            },
+            campaign_description: {
+                type: Sequelize.STRING
+            },
+            campaign_type: {
+                type: Sequelize.STRING
+            },
+            list_order: {
+                type: Sequelize.STRING
+            },
+            list_mix: {
+                type: Sequelize.BOOLEAN
+            },
+            hopper: {
+                type: Sequelize.INTEGER
+            },
+            dial_level: {
+                type: Sequelize.INTEGER
+            },
+            dialtimeout: {
+                type: Sequelize.INTEGER,
+            },
+            active: {
+                allowNull: true,
+                type: Sequelize.STRING,
+                defaultValue: 'Y'
+            },
+            status: {
+                type: Sequelize.STRING,
+                defaultValue: 'Y'
+            },
+            account_id: {
+                type: Sequelize.INTEGER,
+            },
+            agents: {
+                type: Sequelize.JSONB,
+            },
+            params: {
+                type: Sequelize.JSONB,
+            },
+            created_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+                defaultValue: new Date()
+            },
+            max_sales_meet: {
+                type: Sequelize.INTEGER
+            },
+            date_start_meet: {
+                type: Sequelize.STRING
+            },
+            updated_at: {
+                allowNull: true,
+                type: Sequelize.DATE,
+                defaultValue: new Date()
+            },
+            current_strategy: {
+                type: Sequelize.STRING
+            }
         },
-        campaign_name : {
-            type : Sequelize.STRING
-        },
-        campaign_description : {
-            type : Sequelize.STRING
-        },
-        campaign_type : {
-            type : Sequelize.STRING
-        },
-        list_order : {
-            type : Sequelize.STRING
-        },
-        list_mix : {
-            type : Sequelize.BOOLEAN
-        },
-        hopper : {
-            type : Sequelize.INTEGER
-        },
-        dial_level : {
-            type : Sequelize.INTEGER
-        },
-        dialtimeout : {
-            type: Sequelize.INTEGER,
-        },
-        active: {
-            allowNull: true,
-            type: Sequelize.STRING,
-            defaultValue: 'Y'
-        },
-        status: {
-            type: Sequelize.STRING,
-            defaultValue: 'Y'
-        },
-        account_id: {
-            type: Sequelize.INTEGER,
-        },
-        agents: {
-            type: Sequelize.JSONB,
-        },
-        params: {
-            type: Sequelize.JSONB,
-        },
-        created_at : {
-            allowNull: true,
-            type: Sequelize.DATE,
-            defaultValue: new Date()
-        },
-        updated_at : {
-            allowNull: true,
-            type: Sequelize.DATE,
-            defaultValue: new Date()
-        },
-        current_strategy:{
-            type: Sequelize.STRING
-        }},
         {timestamps: false,}
-        );
+    );
 
-        campaign.prototype.fields = [
-            'campaign_id',
-            'campaign_name',
-            'campaign_description',
-            'campaign_type',
-            'list_order',
-            'list_mix',
-            'hopper',
-            'dial_level',
-            'dialtimeout',
-            'created_at',
-            'updated_at',
-            'agents',
-            'status',
-            'params',
-            'current_strategy'
-        ],
+    campaign.prototype.fields = [
+        'campaign_id',
+        'campaign_name',
+        'campaign_description',
+        'campaign_type',
+        'list_order',
+        'list_mix',
+        'hopper',
+        'dial_level',
+        'dialtimeout',
+        'created_at',
+        'updated_at',
+        'agents',
+        'status',
+        'params',
+        'current_strategy',
+        'max_sales_meet',
+        'date_start_meet'
+
+    ],
         campaign.prototype.fieldsSearchMetas = [
             'campaign_id',
             'campaign_name',
@@ -94,13 +104,15 @@ module.exports = (sequelize, Sequelize) => {
             'updated_at',
             'agents',
             'status',
-            'current_strategy'
+            'current_strategy',
+            'max_sales_meet',
+            'date_start_meet'
         ]
-        campaign.associate = function (models) {
-            campaign.belongsTo(models.accounts, {
-                foreignKey: 'account_id'
-            });
-        };
+    campaign.associate = function (models) {
+        campaign.belongsTo(models.accounts, {
+            foreignKey: 'account_id'
+        });
+    };
 
-        return campaign
+    return campaign
 }
