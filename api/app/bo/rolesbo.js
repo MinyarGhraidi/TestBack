ed3fcd90d83bf3f2f0568b913e6bdb32a3ad0991
mohsni tarek <tarak.mohsni@oxilog.net>
2022-10-04 15:53:15 +0200

    update usercurrent token

--- api/app/bo/rolesbo.js
+++ api/app/bo/rolesbo.js
@@ -1,6 +1,7 @@
 const {baseModelbo} = require('./basebo');
 let sequelize = require('sequelize');
 let db = require('../models');
+const appSocket = new (require('../providers/AppSocket'))();
 
 class roles extends baseModelbo {
     constructor() {
@@ -12,8 +13,15 @@ class roles extends baseModelbo {
     isUniqueRole(rolename, account_id, role_id) {
         let _this = this;
         return new Promise((resolve, reject) => {
-            if(role_id !== undefined){
-                this.db['roles'].findAll({where: {role_name: rolename, active: 'Y', account_id: account_id , role_id: { $not: role_id}}})
+            if (role_id !== undefined) {
+                this.db['roles'].findAll({
+                    where: {
+                        role_name: rolename,
+                        active: 'Y',
+                        account_id: account_id,
+                        role_id: {$not: role_id}
+                    }
+                })
                     .then(data => {
                         if (data && data.length !== 0) {
                             resolve(false);
@@ -24,8 +32,8 @@ class roles extends baseModelbo {
                     .catch(err => {
                         reject(err);
                     })
-            }else{
-                this.db['roles'].findAll({where: {role_name: rolename, active: 'Y', account_id: account_id }})
+            } else {
+                this.db['roles'].findAll({where: {role_name: rolename, active: 'Y', account_id: account_id}})
                     .then(data => {
                         if (data && data.length !== 0) {
                             resolve(false);
@@ -44,22 +52,35 @@ class roles extends baseModelbo {
     saveRole(req, res, next) {
         let _this = this;
         let {role_name, account_id, role_id} = req.body;
-        this.isUniqueRole(role_name, account_id ,role_id)
+        this.isUniqueRole(role_name, account_id, role_id)
             .then(isUnique => {
                 if (isUnique) {
                     if (role_id) {
-                        this.db['roles'].update(req.body, {where: {role_id: role_id},
+                        this.db['roles'].update(req.body, {
+                            where: {role_id: role_id},
                             returning: true,
-                            plain: true})
+                            plain: true
+                        })
                             .then(role => {
-                                    if (role){
-                                        res.send({
-                                            status: 200,
-                                            success: true,
-                                            message: 'Success',
-                                            data: role
-                                        })
-                                    }
+                                if (role) {
+                                    this.updateUserToken(role).then(result => {
+                                        if (result){
+                                            res.send({
+                                                status: 200,
+                                                success: true,
+                                                message: 'Success',
+                                                data: role
+                                            })
+                                        }else{
+                                            res.send({
+                                                success: false,
+                                                message: 'error update',
+                                            })
+                                        }
+
+                                    })
+
+                                }
                             })
                             .catch(err => {
                                 return _this.sendResponseError(res, ['cannot update role', err], 1, 403);
@@ -68,7 +89,7 @@ class roles extends baseModelbo {
                         let modalObj = this.db['roles'].build(req.body)
                         modalObj.save()
                             .then(role => {
-                                if(role){
+                                if (role) {
                                     res.send({
                                         status: 200,
                                         success: true,
@@ -98,23 +119,25 @@ class roles extends baseModelbo {
         let id = req.params.params;
 
         this.db['roles'].update({
-            active: 'N'
+                active: 'N'
             },
-            {where: {
-                role_id: id
-                }}
+            {
+                where: {
+                    role_id: id
+                }
+            }
         ).then(result => {
             if (result) {
                 this.db['acls'].destroy({
-                    where :{
+                    where: {
                         role_id: id,
                     }
-                }).then(result =>{
+                }).then(result => {
                     res.json({
                         success: true,
                         messages: 'deleted'
                     })
-                }).catch(err =>{
+                }).catch(err => {
                     return this.sendResponseError(res, ['Error', err], 1, 403);
                 })
 
@@ -128,6 +151,58 @@ class roles extends baseModelbo {
             res.status(500).json(err)
         )
 
+    }
+
+    updateUserToken(role) {
+        return new Promise((resolve, reject) => {
+            this.db['users'].update({
+                current_session_token: null
+            },{
+                where: {
+                    role_id: role[1].role_id,
+                    active: 'Y',
+                    status: 'Y'
+                }
+            }).then(user => {
+                if (user) {
+                    this.db['users'].findAll({
+                        where:{
+                            role_id: role[1].role_id,
+                            active: 'Y',
+                            status: 'Y'
+                        }
+                    }).then(userToken=>{
+                        if(userToken && userToken.length !==0){
+
+                            let index = 0;
+                            userToken.forEach(item => {
+                                appSocket.emit('reload.Permission', {user_id: item.user_id});
+                                if (index < user.length - 1) {
+                                    index++;
+                                } else {
+                                    resolve({
+                                        success: true
+                                    })
+                                }
+                            })
+                        } else{
+                            resolve({
+                                success: true
+                            })
+                        }
+                    }).catch(err=>{
+                        reject(err)
+                    })
+
+                } else {
+                    resolve({
+                        success: true
+                    })
+                }
+            }).catch(err => {
+                reject(err)
+            })
+        })
     }
 
 
