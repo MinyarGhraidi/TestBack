5c9d0536bb60d23b69a943163e98ddca3a90b441
mohsni tarek <tarak.mohsni@oxilog.net>
2023-01-23 17:17:55 +0100

    call calendar

--- api/app/models/reminders.model.js
+++ api/app/models/reminders.model.js
@@ -15,7 +15,7 @@ module.exports =(sequelize, Sequelize) =>{
             type: Sequelize.INTEGER
         },
         date:{
-            type: Sequelize.DATE
+            type: Sequelize.STRING
         },
         time:{
             type: Sequelize.STRING
