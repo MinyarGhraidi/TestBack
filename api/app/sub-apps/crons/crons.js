fa23abcd760ea50dd0629b99732be802e8b6c697
oussama <oussama.saad@oxilog.net>
2023-03-06 14:07:06 +0100

    add play media record

--- api/app/sub-apps/crons/crons.js
+++ api/app/sub-apps/crons/crons.js
@@ -1,23 +1,32 @@
 const AddCallFile = require("./addCallFile");
 const AddReminder = require("./addReminder");
+const MigrateRecords = require("./migrateRecords");
 let Cron = require('cron').CronJob;
 let addCF = new AddCallFile();
 let addReminder = new AddReminder();
+let migrateRecords = new MigrateRecords();
 
 
 
+//
+// let Add_CallFiles = new Cron("* * * * *", async function () {
+//     addCF.cronListCallFiles().then(result => {
+//         console.log(result)
+//     });
+// }, null, true, 'Europe/Paris');
+//
+// let Add_Reminder = new Cron("* * * * *", async function () {
+//     addReminder.saveNotificationReminder().then(result => {
+//         console.log(result)
+//     });
+// }, null, true, 'Europe/Paris');
 
-let Add_CallFiles = new Cron("* * * * *", async function () {
-    addCF.cronListCallFiles().then(result => {
+let Migrate_records = new Cron("* * * * * *", async function () {
+    migrateRecords.migrateRecords().then(result => {
         console.log(result)
     });
 }, null, true, 'Europe/Paris');
-
-let Add_Reminder = new Cron("* * * * *", async function () {
-    addReminder.saveNotificationReminder().then(result => {
-        console.log(result)
-    });
-}, null, true, 'Europe/Paris');
-
-Add_CallFiles.start();
-Add_Reminder.start();
+//
+// Add_CallFiles.start();
+// Add_Reminder.start();
+Migrate_records.start()
