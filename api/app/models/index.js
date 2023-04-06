cdb508e51536f40585fec969b81c310c8a42119b
oussama <oussama.saad@oxilog.net>
2023-04-06 10:09:34 +0100

    clean code

--- api/app/models/index.js
+++ api/app/models/index.js
@@ -48,6 +48,7 @@ fs.readdirSync(__dirname)
       return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(file => {
+        console.log(sequelize['crm-app']['import'])
       const model = sequelize['crm-app']['import'](path.join(__dirname, file));
       db[model.name] = model;
     });
