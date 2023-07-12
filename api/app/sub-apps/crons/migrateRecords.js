69ea8fa8925999b3a1ab24981a566c30d7397ad2
Oussama SAAD <oussama.saad@oxilog.net>
2023-07-12 07:13:27 +0000

    Feature/test code

--- api/app/sub-apps/crons/migrateRecords.js
+++ api/app/sub-apps/crons/migrateRecords.js
@@ -16,7 +16,7 @@ class MigrateRecords extends baseModelbo {
                                        where is_treated = 'N'
                                          AND record_url <> ''
                                          and durationsec <> '0'
-                                         and start_time like :current_date limit 50`
+                                         and start_time like :current_date limit 100`
             db.sequelize['cdr-db'].query(SqlGetCDrNotTreated, {
                 type: db.sequelize['cdr-db'].QueryTypes.SELECT,
                 replacements: {
@@ -69,56 +69,7 @@ class MigrateRecords extends baseModelbo {
 
                                     })
                                 })  .catch(error => {
-
-                                let index = item_cdr.record_url.indexOf('.')
-                                let pathTelco = item_cdr.record_url.slice(0, index+1) + "+" + item_cdr.record_url.slice(index+1, item_cdr.record_url.length);
-                                console.log(index, pathTelco)
-
-                                client.downloadFile(
-                                    pathTelco,
-                                    '/var/www/crm/crm-backend/api/app/recordings/' + item_cdr.memberUUID + '.wav',
-                                )
-                                    .then(response => {
-                                        let cmd_ffmpeg = ' ffmpeg -i /var/www/crm/crm-backend/api/app/recordings/' + item_cdr.memberUUID + '.wav -vn -ar 44100 -ac 2 -b:a 192k ' + '/var/www/crm/crm-backend/api/app/recordings/' + item_cdr.memberUUID + '.mp3'
-                                        exec(cmd_ffmpeg, (error, stdout, stderr) => {
-                                            let SqlUpdateTreated = ` update acc_cdrs
-                                                                    set is_treated= 'Y'
-                                                                   where
-                                                                     id = :id `
-                                            db.sequelize['cdr-db'].query(SqlUpdateTreated, {
-                                                type: db.sequelize['cdr-db'].QueryTypes.SELECT,
-                                                replacements: {
-                                                    id: item_cdr.id
-                                                }
-                                            }).then(updateCdr => {
-                                                this.db["calls_historys"].update({record_url: 'https://api.skycrm360.io/api/callHistory/play/' + item_cdr.memberUUID}, {
-                                                    where: {
-                                                        uuid: item_cdr.memberUUID,
-                                                        active: 'Y'
-                                                    }
-                                                })
-                                                    .then(() => {
-                                                        let cmd_delete = 'rm -rf ' + '/var/www/crm/crm-backend/api/app/recordings/' + item_cdr.memberUUID + '.wav'
-                                                        exec(cmd_delete, (error, stdout, stderr) => {
-                                                            if (index <= datacdrRecords.length - 1) {
-                                                                index++
-                                                            } else {
-                                                                resolve(true)
-                                                            }
-                                                        })
-                                                    }).catch(error => {
-                                                    console.log(error)
-                                                })
-
-                                            }).catch(error => {
-                                                console.log(error)
-                                            });
-
-                                        })
-                                    })  .catch(error => {
-
-                                    console.log(error)
-                                })
+                                    console.log('err', error)
                             })
                         })
                     }).catch(e => console.log(e))
