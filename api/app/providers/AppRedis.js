c87db30f63d4e17ccdfadfb391797b79c59dad50
mostapha.ghribi <mostapha.ghribi@oxilog.net>
2023-07-27 08:29:51 +0100

    remove comment

--- api/app/providers/AppRedis.js
+++ api/app/providers/AppRedis.js
@@ -4,7 +4,7 @@ const { appConfig } = require("../helpers/app");
 class AppRedis {
     constructor() {
         console.log(appConfig.redis)
-        //this.client = redis.createClient({url: 'redis://'+appConfig.redis.host+':'+appConfig.redis.port});
+        this.client = redis.createClient({url: 'redis://'+appConfig.redis.host+':'+appConfig.redis.port});
     }
 }
 
