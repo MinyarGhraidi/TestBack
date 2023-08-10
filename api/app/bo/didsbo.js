079244f505bdd12e05f9b8fa4ed07bbd0fb0e06f
mohsni tarek <tarak.mohsni@oxilog.net>
2023-08-10 12:56:56 +0200

    delete dids

--- api/app/bo/didsbo.js
+++ api/app/bo/didsbo.js
@@ -27,20 +27,19 @@ class dids extends baseModelbo {
 
     deleteDiD (req, res, next){
        let dids = req.body;
-        if (!!!dids) {
+        if (!!!dids.did_group_id || (dids.number && dids.number.length === 0) ) {
             this.sendResponseError(res, ['data_is_required'])
             return
         }
-        let did_group_id = dids[0].did_group_id
-        let number = dids.map(item=> item.number)
+
                 let sql = `UPDATE dids as did
                     set active = 'N'
                     where number in (:number) and did.did_group_id = :did_group_id `
                 db.sequelize['crm-app'].query(sql, {
                     type: db.sequelize['crm-app'].QueryTypes.SELECT,
                     replacements: {
-                        did_group_id:did_group_id,
-                        number: number
+                        did_group_id:dids.did_group_id,
+                        number: dids.number
                     }
                 }).then(result=>{
                         res.send({
